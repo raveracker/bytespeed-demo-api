e8ad10bb560e4a134ba425839bbeeb7866828a07
Allan Joseph <allanjeo95@gmail.com>
2023-08-25 04:42:40 +0530

    changed postgres setup to render

--- src/db.ts
+++ src/db.ts
@@ -2,9 +2,9 @@ import { Pool } from "pg";
 
 const db = new Pool({
   user: "bytespeed",
-  host: "localhost",
-  database: "bytespeed_demo_db",
-  password: "bytespeed",
+  host: "dpg-cjju5l337aks73dgd6pg-a",
+  database: "bytespeed",
+  password: "4iUlxiD5s3NDRxsft6KAaoXyDQhfIUeE",
   port: 5432, // Default PostgreSQL port
 });
 
